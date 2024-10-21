ddf7fe90ffc56b20d26ac6451c78a88d644e77a6
ADRIAN FERNANDO MARTINS <adrian.fmartins@senacsp.edu.br>
2024-10-21 16:21:56 -0300

    v. 1.0

---
+++ backup.bat
@@ -0,0 +1,4 @@
+mkdir backup
+copy \Windows\*.txt \Users\adrian.fmartins\Documents\script-backup\backup
+copy \Windows\System32\*.log \Users\adrian.fmartins\Documents\script-backup\backup
+copy \Windows\System32\*.txt \Users\adrian.fmartins\Documents\script-backup\backup
